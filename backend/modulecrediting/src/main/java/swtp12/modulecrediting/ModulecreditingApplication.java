6b27b39f72a90e6f9d2c0f41098f0db2c34bbd4c
llkippe <102752683+llkippe@users.noreply.github.com>
2023-11-03 15:35:09 +0100

    setup backend

---
+++ backend/modulecrediting/src/main/java/swtp12/modulecrediting/ModulecreditingApplication.java
@@ -0,0 +1,13 @@
+package swtp12.modulecrediting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModulecreditingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModulecreditingApplication.class, args);
+	}
+
+}
---
+++ backend/modulecrediting/src/test/java/swtp12/modulecrediting/ModulecreditingApplicationTests.java
@@ -0,0 +1,13 @@
+package swtp12.modulecrediting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModulecreditingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
