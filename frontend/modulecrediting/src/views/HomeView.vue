d99d5d0ab7794fd9bf88f7bc5db64aba206d4646
Finn Kuschetzki <finnmail@icloud.com>
2023-11-16 11:21:58 +0100

    frontend cleanup

--- frontend/modulecrediting/src/views/HomeView.vue
+++ frontend/modulecrediting/src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <p>HomeView</p>
 </template>
+
+<style scoped>
+</style>
