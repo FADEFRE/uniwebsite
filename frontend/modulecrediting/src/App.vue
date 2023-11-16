55fd067b0eadff3cdd92b004561f0989ead753de
Finn Kuschetzki <finnmail@icloud.com>
2023-11-16 15:22:50 +0100

    added SubmitApplicationView and ModuleApplicationPanel with dummy route

--- frontend/modulecrediting/src/App.vue
+++ frontend/modulecrediting/src/App.vue
@@ -4,11 +4,43 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <template>
   <header>
-    <p>Header</p>
+    <div class="header-container">
+
+      <img src="./assets/Universität_Leipzig_logo.svg" alt="Logo der Universität Leipzig" class="logo"/>
+
+      <div class="router-container">
+        <router-link to="/" class="router-button">Antrag</router-link>
+        <router-link to="/test" class="router-button">Status</router-link>
+      </div>
+
+    </div>
   </header>
 
   <RouterView />
 </template>
 
 <style scoped>
+.header-container {
+  height: 7vh;
+  display: flex;
+}
+
+.logo {
+  height: 100%;
+}
+
+.router-container {
+  display: inline;
+  justify-content: right;
+}
+
+.router-button {
+  display: inline-block;
+  padding: 10px 20px;
+  background-color: #3498db; /* Set your desired background color */
+  color: #fff; /* Set your desired text color */
+  text-decoration: none;
+  border-radius: 5px;
+  transition: background-color 0.3s ease;
+}
 </style>
--- frontend/modulecrediting/src/router/index.js
+++ frontend/modulecrediting/src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import SubmitApplicationView from '../views/SubmitApplicationView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,8 +7,13 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: SubmitApplicationView
     },
+    {
+      path: '/test',
+      name: 'test',
+      component: SubmitApplicationView
+    }
   ]
 })
 
