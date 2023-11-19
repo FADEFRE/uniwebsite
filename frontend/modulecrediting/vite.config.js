8e0fd36c064f29d741233e29a18a90650bdf93af
Jeremias Aechtner <qq69omox@studserv.uni-leipzig.de>
2023-11-19 19:09:29 +0100

    .

--- frontend/modulecrediting/vite.config.js
+++ frontend/modulecrediting/vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  server: {
-    port: 8080,
-  },
+// server: {
+//    port: 8080,
+//  },
 })
