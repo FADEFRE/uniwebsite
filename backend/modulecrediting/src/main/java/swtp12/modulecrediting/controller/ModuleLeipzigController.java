c720c3968fcd0a5f5863b1312800379e11405e0d
Frederik Kluge <frederikkluge@gmail.com>
2023-11-22 04:25:26 +0100

    vll useless, didnt want to delete (code in comment)

--- backend/modulecrediting/src/main/java/swtp12/modulecrediting/controller/ModuleLeipzigController.java
+++ backend/modulecrediting/src/main/java/swtp12/modulecrediting/controller/ModuleLeipzigController.java
@@ -2,14 +2,23 @@
 package swtp12.modulecrediting.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import swtp12.modulecrediting.model.CourseLeipzig;
 import swtp12.modulecrediting.model.ModuleLeipzig;
+import swtp12.modulecrediting.repository.CourseLeipzigRepository;
 import swtp12.modulecrediting.repository.ModuleLeipzigRepository;
 
 @RestController
@@ -20,8 +29,55 @@ public class ModuleLeipzigController {
     @Autowired
     private ModuleLeipzigRepository modulLeipzigRepository;
 
+    @Autowired
+    private CourseLeipzigRepository courseLeipzigRepository;
+
+
     @GetMapping
     List<ModuleLeipzig> getModulesLeipzig() {
         return modulLeipzigRepository.findAll();
     }
+
+
+
+
+
+    /* 
+    @GetMapping("/module-leipzig/{id}")
+    public ResponseEntity<ModuleLeipzig> getModuleLeipzigById(@PathVariable("id") Long id) {
+        Optional<ModuleLeipzig> moduleLeipzig = modulLeipzigRepository.findById(id);
+        if(moduleLeipzig.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(moduleLeipzig.get(), HttpStatus.OK);
+    }
+
+    @PostMapping("/course-leipzig/{courseLeipzigId}/module-leipzig")
+    public ResponseEntity<ModuleLeipzig> createModuleLeipzig(@PathVariable("courseLeipzigId") Long id, @RequestBody ModuleLeipzig reqModuleLeipzig) {
+        Optional<CourseLeipzig> optionalCourse = courseLeipzigRepository.findById(id);
+        if (optionalCourse.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        CourseLeipzig courseLeipzig = optionalCourse.get();
+        Optional<ModuleLeipzig> resModuleLeipzig = modulLeipzigRepository.findById(id);
+        ModuleLeipzig moduleLeipzig;
+        if (resModuleLeipzig.isEmpty()) {
+            moduleLeipzig = modulLeipzigRepository.save(new ModuleLeipzig(reqModuleLeipzig.getModuleName(), reqModuleLeipzig.getModuleCode(), courseLeipzig.getName()));
+            courseLeipzig.addCourseToModulesLeipzig(moduleLeipzig);
+            courseLeipzigRepository.save(courseLeipzig);
+            return new ResponseEntity<>(moduleLeipzig, HttpStatus.CREATED);
+        }
+        else {
+            courseLeipzig.addCourseToModulesLeipzig(resModuleLeipzig.get());
+            courseLeipzigRepository.save(courseLeipzig);
+            return new ResponseEntity<>(resModuleLeipzig.get(), HttpStatus.OK);
+        }
+    }
+
+    @DeleteMapping("module-leipzig/{id}")
+    public ResponseEntity<HttpStatus> deleteModuleLeipzigById(@PathVariable("id") Long id) {
+        modulLeipzigRepository.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+    */
 }
