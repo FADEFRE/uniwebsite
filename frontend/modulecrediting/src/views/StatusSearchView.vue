71c78ad24ae0eb34e37b8880619661467835b871
Finn Kuschetzki <finnmail@icloud.com>
2023-11-25 00:10:51 +0100

    added existence check for application id in StatusSearchView #34

--- frontend/modulecrediting/src/views/StatusSearchView.vue
+++ frontend/modulecrediting/src/views/StatusSearchView.vue
@@ -1,20 +1,33 @@
 <script setup>
 import { useRouter } from 'vue-router'
 import { ref } from 'vue'
+import { url } from "@/scripts/url-config";
+import axios from "axios";
 
 const router = useRouter()
 
+const isInvalid = ref(false)
+
 const id = ref()
 const openDetailView = () => {
-  const routeData = router.resolve({name: 'statusDetail', params: {id: id.value}})
-  window.open(routeData.href, '_top')
+  console.log(url + `/applications/${id.value}/exists`)
+  axios.get(url + `/applications/${id.value}/exists`)
+      .then(response => {
+        if (response.data) {
+          const routeData = router.resolve({name: 'statusDetail', params: {id: id.value}})
+          window.open(routeData.href, '_top')
+        } else {
+          isInvalid.value = true
+        }
+      })
+  // todo error catching
 }
 </script>
 
 <template>
   <div class="view-container">
     <h2>Vorgangsnummer eingeben:</h2>
-    <InputText type="text" v-model="id"/>
+    <InputText type="text" v-model="id" :class="{ 'p-invalid': isInvalid }"/>
     <Button @click="openDetailView">Status einsehen</Button>
   </div>
 </template>
