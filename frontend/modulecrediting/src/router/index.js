94d2f56388c7f71b02bac7a031e3050f1eb935b1
Finn Kuschetzki <finnmail@icloud.com>
2023-11-24 20:56:03 +0100

    added StatusSearchView

--- frontend/modulecrediting/src/router/index.js
+++ frontend/modulecrediting/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import SubmitApplicationView from '../views/SubmitApplicationView.vue'
+import StatusSearchView from "@/views/StatusSearchView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,9 +11,9 @@ const router = createRouter({
       component: SubmitApplicationView
     },
     {
-      path: '/test',
-      name: 'test',
-      component: SubmitApplicationView
+      path: '/status',
+      name: 'status',
+      component: StatusSearchView
     }
   ]
 })
---
+++ frontend/modulecrediting/src/views/StatusSearchView.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <h2>Vorgangsnummer eingeben:</h2>
+    <InputText type="text" />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
