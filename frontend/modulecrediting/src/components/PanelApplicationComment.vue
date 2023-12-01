d335eb40c2ab28f9d27f4df81ba7afbc514f6401
Toan Anh Nguyen <toananh988@gmail.com>
2023-12-01 17:21:48 +0100

    PanelApplicationComment updated

--- frontend/modulecrediting/src/components/PanelApplicationComment.vue
+++ frontend/modulecrediting/src/components/PanelApplicationComment.vue
@@ -1,44 +1,44 @@
-<!--
-comment input
-displays:
-- comment text input
-functionality:
-- comment input
--->
+<template>
+  <div>
+    <textarea class="module-comment" v-model="applicantComment" ref="commentInput" @input="adjustTextareaHeight" />
+  </div>
+</template>
 
 <script setup>
 import { ref } from 'vue';
 
-const applicantComment = ref()
+const applicantComment = ref('');
+const commentInput = ref(null);
+
+const adjustTextareaHeight = () => {
+  const textarea = commentInput.value;
+  textarea.style.height = 'auto'; // Resetting to auto height
+  textarea.style.height = `${textarea.scrollHeight}px`; // Setting height based on content
+};
 
 defineExpose({
   applicantComment
 })
 </script>
 
-<template>
-  <div>
-    <textarea class="module-comment" v-model="applicantComment"/>
-  </div>
-</template>
-
 <style scoped>
 .module-comment {
-  padding: 5px;
-  margin: 5px;
-
-  width: 100%;
-  height: 100%;
-  font-size: 16px;
+  padding: 2.5px;
+  margin: 2.5px auto;
+  /* Zentriert das Textfeld horizontal */
+  width: 90%;
+  min-height: 50px;
+  font-size: 12px;
   color: #333333;
-
   box-shadow: 0 0 5px rgba(0, 128, 255, 0);
   border: 1px solid #aaaaaa;
   border-radius: 5px;
   transition: border-color 0.2s;
+  resize: none;
 }
 
-.module-comment:hover, .module-comment:focus {
+.module-comment:hover,
+.module-comment:focus {
   border-color: black;
 }
 </style>
