c7f5c68bfbac5be3773c770b1773d11640ab256c
llkippe <102752683+llkippe@users.noreply.github.com>
2023-12-15 03:33:00 +0100

    changed url without env var !!! check when running locally

--- frontend/modulecrediting/src/scripts/url-config.js
+++ frontend/modulecrediting/src/scripts/url-config.js
@@ -1,3 +1,4 @@
-const url = import.meta.env.VITE_URL || "http://localhost:8090"
+//const url = import.meta.env.VITE_URL || "http://localhost:8090"
+const url = "http://172.26.92.91:8090"
 
 export { url }
