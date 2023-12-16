34820c9b193f24fe8140c57a90d459c58bf14855
Frederik Kluge <fk13mami@studserv.uni-leipzig.de>
2023-12-16 16:46:06 +0100

    Changed User Class

---
+++ backend/modulecrediting/src/main/java/swtp12/modulecrediting/model/EnumUserRole.java
@@ -0,0 +1,7 @@
+package swtp12.modulecrediting.model;
+
+public enum EnumUserRole {
+    STUDY_OFFICE,
+    CHAIRMAN,
+    ADMIN
+}
--- backend/modulecrediting/src/main/java/swtp12/modulecrediting/model/User.java
+++ backend/modulecrediting/src/main/java/swtp12/modulecrediting/model/User.java
@@ -1,10 +1,9 @@
 package swtp12.modulecrediting.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -13,17 +12,17 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @Entity
-@Table(name = "app_user")
+@Table(name = "app-user")
 public class User {
     @Id
     @GeneratedValue
     private Long id;
     private String username;
     private String password;
-    private String role; //enum?
+    private EnumUserRole role;
 
 
-    public User(String username, String password, String role) {
+    public User(String username, String password, EnumUserRole role) {
         this.username = username;
         this.password = password;
         this.role = role;
