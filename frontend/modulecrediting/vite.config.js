e8be097ec78253a15d7a08e46d2db4ba7876043a
llkippe <102752683+llkippe@users.noreply.github.com>
2023-12-19 00:02:49 +0100

    changed vite conifg to chairman state

--- frontend/modulecrediting/vite.config.js
+++ frontend/modulecrediting/vite.config.js
@@ -5,17 +5,12 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-    vue()
-    ],
-    build: {
-        rollupOptions: {
-            external: ['axios'],
-        },
-    },
-    resolve: {
+  plugins: [
+    vue(),
+  ],
+  resolve: {
     alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-    }
+  }
 })
