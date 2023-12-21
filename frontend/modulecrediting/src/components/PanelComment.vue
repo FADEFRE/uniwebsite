00fa7c57f3b1462f80c0177f105c3b351ca73ab4
Finn Kuschetzki <finnmail@icloud.com>
2023-12-21 23:06:27 +0100

    refactored PanelComment #118

--- frontend/modulecrediting/src/components/PanelComment.vue
+++ frontend/modulecrediting/src/components/PanelComment.vue
@@ -1,37 +1,35 @@
 <!--
 comment input
+props:
+  - type (may be 'edit' or 'readonly')
+  - comment (optional)
+exposes:
+  - comment
 displays:
-- comment text input
-functionality:
-- comment input
+  - commentary heading
+  - textarea element
 -->
-
 
 <script setup>
 import { ref } from 'vue';
 
-const props = defineProps(['readOnly', 'comment'])
-
-const readOnly = ref(props.readOnly ? props.readOnly : false)
+const props = defineProps({
+  type: {
+    required: true,
+    type: String,
+    validator(value) {
+      return ['edit', 'readonly'].includes(value)
+    }
+  },
+  comment: {
+    type: String
+  }
+})
 
 const comment = ref('');
 if (props.comment) {
   comment.value = props.comment
 }
-
-const commentInput = ref(null);
-const fontSize = ref(16); // Fest auf 16 Pixel gesetzte Schriftgröße
-
-const adjustTextareaHeight = () => {
-  const textarea = commentInput.value;
-  textarea.style.height = 'auto'; // Resetting to auto height
-  textarea.style.height = `${textarea.scrollHeight}px`; // Setting height based on content
-};
-
-const adjustFontSize = () => {
-  const textarea = commentInput.value;
-  textarea.style.fontSize = `${fontSize.value}px`; // Setting font size
-};
 
 defineExpose({
   comment
@@ -40,29 +38,15 @@ defineExpose({
 
 <template>
   <div>
-    <textarea :readonly="readOnly" class="module-comment" v-model="comment" ref="commentInput" @input="adjustTextareaHeight" />
+    <h3>Kommentar</h3>
+    <textarea
+        :readonly="type === 'new' || type === 'edit'"
+        rows="3"
+        v-model="comment"
+    />
   </div>
 </template>
 
 <style scoped>
-.module-comment {
-  padding: 2.5px;
-  margin: 2.5px auto;
-  /* Zentriert das Textfeld horizontal */
-  width: 90%;
-  min-height: 50px;
-  font-size: 16px;
-  /* Fest auf 16 Pixel gesetzte Schriftgröße */
-  color: #333333;
-  box-shadow: 0 0 5px rgba(0, 128, 255, 0);
-  border: 1px solid #aaaaaa;
-  border-radius: 5px;
-  transition: border-color 0.2s;
-  resize: none;
-}
 
-.module-comment:hover,
-.module-comment:focus {
-  border-color: black;
-}
 </style>
