5265d48fe158693f0614ee99c3b7483581b75ebb
Finn Kuschetzki <finnmail@icloud.com>
2023-12-21 13:56:39 +0100

    added PanelDecision and PanelDecisionBlock components #120 #121

---
+++ frontend/modulecrediting/src/components/PanelDecision.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <h2>Entscheidung</h2>
+    <slot name="study-office" />
+    <slot name="chairman" />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ frontend/modulecrediting/src/components/PanelDecisionBlock.vue
@@ -0,0 +1,59 @@
+<script setup>
+import { ref } from "vue";
+
+const props = defineProps({
+  type: {
+    required: true,
+    type: String,
+    validator(value) {
+      return ['edit', 'readonly'].includes(value)
+    }
+  },
+  displayDecision: {
+    type: String,
+    validator(value) {
+      return ['accept', 'asExamCertificate', 'denied'].includes(value)
+    }
+  },
+  comment: {
+    type: String
+  }
+})
+
+const decision = ref()
+const decisionOptions = ['Annehmen', 'Übungsschein', 'Ablehnen']
+</script>
+
+<!-- todo accessibility -->
+<template>
+  <div>
+
+    <div>
+      <div v-if="type === 'edit'">
+        <SelectButton :allow-empty="false" v-model="decision" :options="decisionOptions" />
+      </div>
+
+      <div v-else-if="type === 'readonly'">
+
+        <img v-if="displayDecision === 'accept'" src="../assets/icons/ModuleAccepted.svg">
+        <img v-else src="../assets/icons/ModuleAcceptedGray.svg">
+
+        <img v-if="displayDecision === 'asExamCertificate'" src="../assets/icons/ModuleAsExamCertificate.svg">
+        <img v-else src="../assets/icons/ModuleAsExamCertificateGray.svg">
+
+        <img v-if="displayDecision === 'denied'" src="../assets/icons/ModuleDenied.svg">
+        <img v-else src="../assets/icons/ModuleDeniedGray.svg">
+
+      </div>
+    </div>
+
+    <div>
+      <textarea :readonly="type === 'readonly'">{{ comment }}</textarea>
+    </div>
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
