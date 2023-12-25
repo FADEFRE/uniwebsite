6b2d162e32f6e75baba98ec2e43e3587d888cf35
jf72muti <jf72muti@studserv.uni-leipzig.de>
2023-12-25 10:45:15 +0100

    pdf generator new Structure  #45 #146

--- backend/modulecrediting/src/main/java/swtp12/modulecrediting/service/GeneratedPdfService.java
+++ backend/modulecrediting/src/main/java/swtp12/modulecrediting/service/GeneratedPdfService.java
@@ -20,7 +20,7 @@ import swtp12.modulecrediting.model.ModulesConnection;
 public class GeneratedPdfService {
     @Autowired
     private ApplicationService applicationService;
-        
+
     public byte[] generatePdfDataDocument(String id) throws IOException, DocumentException {
         Application application = applicationService.getApplicationById(id);
         List<ModulesConnection> modulesConnections = application.getModulesConnections();
@@ -31,82 +31,130 @@ public class GeneratedPdfService {
         Document document = new Document(PageSize.A4);
         PdfWriter.getInstance(document, baos);
         document.open();
+        document.newPage();
 
-        for (int i = 0; i < modulesConnections.size(); i++) {
+        //uni-leipzig icon
+        Image uniLeipzigIcon = Image.getInstance(Objects.requireNonNull(getClass().getResource("/Universität_Leipzig_Logo.png")));
+        float maxWidth = 300f;
+        float maxHeight = 300f;
+        uniLeipzigIcon.scaleToFit(maxWidth, maxHeight);
+        float iconX = 36;
+        float iconY = PageSize.A4.getHeight() - 36 - uniLeipzigIcon.getScaledHeight();
+        uniLeipzigIcon.setAbsolutePosition(iconX, iconY);
+        document.add(uniLeipzigIcon);
+
+        //Title
+        String titleText = "ANTRAG ZUR MODULANRECHNUNG";
+        String titleText1 = "FAKULTÄT MATHEMATIK UND INFORMATIK";
+        Font titleFont = getFont(HELVETICA_BOLD, 15);
+        Font titleFont1 = getFont(HELVETICA, 14);
+        Paragraph title = new Paragraph(titleText, titleFont);
+        Paragraph title1 = new Paragraph(titleText1, titleFont1);
+        title.setAlignment(Element.ALIGN_LEFT);
+        title1.setAlignment(Element.ALIGN_LEFT);
+        title.setIndentationLeft(30f);
+        title1.setIndentationLeft(30f);
+        float Spacing = 150f; // Abstand zwischen Icon und Titel
+        title.setSpacingBefore(Spacing);
+        title1.setSpacingAfter(30);
+        document.add(title);
+        document.add(title1);
+
+        // Generelle Antragsdaten
+        addTable(document, "Vorgangsnummer", String.valueOf(id));
+        addTable(document, "Status", (application.getFullStatus() != null) ? application.getFullStatus().toString() : "");
+        addTable(document, "Erstellungsdatum", (application.getCreationDate() != null) ? application.getCreationDate().toString() : "");
+        addTable(document, "Entscheidungsdatum", (application.getDecisionDate() != null) ? application.getDecisionDate().toString() : "");
+
+        String addressText = "Universität Leipzig\n" +
+                "Studienbüro Fakultät für Mathematik und Informatik\n" +
+                "Neues Augusteum\n" +
+                "04109 Leipzig\n\n" +
+                "Leitung: Marco Neumann\n\n" +
+                "Telefon: +49 341 97-32165\n" +
+                "Telefax: +49 341 97-32193\n" +
+                "Email-Adresse: studienbuero@math.uni-leipzig.de\n\n" +
+                "Die aktuellen Sprechzeiten entnehmen\n" +
+                "Sie bitte der Homepage:\n" +
+                "www.mathcs.uni-leipzig.de/studium/studienbuero\n";
+
+        Font addressFont = getFont(HELVETICA, 10);
+        PdfPTable addressTable = new PdfPTable(1);
+        addressTable.setWidthPercentage(88);
+        addressTable.getDefaultCell().setBorder(Rectangle.NO_BORDER);
+
+        PdfPCell addressCell = new PdfPCell(new Paragraph(addressText, addressFont));
+        addressCell.setHorizontalAlignment(Element.ALIGN_LEFT);
+        addressCell.setBorder(Rectangle.NO_BORDER);
+
+
+        addressTable.addCell(addressCell);
+        addressTable.setSpacingBefore(50f);
+        document.add(addressTable);
+
+
+        for (int i = 0; i < modulesConnections.size(); i += 2) {
             document.newPage();
-            ModulesConnection modulesConnection = modulesConnections.get(i);
-            ModuleApplication moduleApplication = modulesConnection.getModuleApplication();
+
+            ModulesConnection modulesConnection1 = modulesConnections.get(i);
+            ModuleApplication moduleApplication1 = modulesConnection1.getModuleApplication();
 
             //uni-leipzig icon
-            Image uniLeipzigIcon = Image.getInstance(Objects.requireNonNull(getClass().getResource("/Universität_Leipzig_Logo.png")));
-            float maxWidth = 300f;
-            float maxHeight = 300f;
-            uniLeipzigIcon.scaleToFit(maxWidth, maxHeight);
-            float iconX = 36;
-            float iconY = PageSize.A4.getHeight() - 36 - uniLeipzigIcon.getScaledHeight();
-            uniLeipzigIcon.setAbsolutePosition(iconX, iconY);
             document.add(uniLeipzigIcon);
 
             //Title
-            String titleText = "ANTRAG ZUR MODULANRECHNUNG";
-            String titleText1 = "FAKULTÄT MATHEMATIK UND INFORMATIK";
-            Font titleFont = getFont(HELVETICA_BOLD, 15);
-            Font titleFont1 = getFont(HELVETICA, 14);
-            Paragraph title = new Paragraph(titleText, titleFont);
-            Paragraph title1 = new Paragraph(titleText1, titleFont1);
-            title.setAlignment(Element.ALIGN_LEFT);
-            title1.setAlignment(Element.ALIGN_LEFT);
-            title.setIndentationLeft(30f);
-            title1.setIndentationLeft(30f);
-            float Spacing = 150f; // Abstand zwischen Icon und Titel
-            title.setSpacingBefore(Spacing);
-            title1.setSpacingAfter(50);
-            document.add(title);
-            document.add(title1);
-
-            // Generelle Antragsdaten
-            addTable(document, "Vorgangsnummer", String.valueOf(id));
-            addTable(document, "Status", (application.getFullStatus() != null) ? application.getFullStatus().toString() : "");
-            addTable(document, "Erstellungsdatum", (application.getCreationDate() != null) ? application.getCreationDate().toString() : "");
-            addTable(document, "Entscheidungsdatum", (application.getDecisionDate() != null) ? application.getDecisionDate().toString() : "");
-
+            String titleText2 = "MODUL";
+            Font titleFont2 = getFont(HELVETICA_BOLD, 15);
+            Font titleFont3 = getFont(HELVETICA, 14);
+            Paragraph title2 = new Paragraph(titleText2, titleFont2);
+            Paragraph title3 = new Paragraph(moduleApplication1.getName().toUpperCase(), titleFont3);
+            title2.setAlignment(Element.ALIGN_LEFT);
+            title3.setAlignment(Element.ALIGN_LEFT);
+            title2.setIndentationLeft(30f);
+            title3.setIndentationLeft(30f);
+            float Spacing1 = 150f; // Abstand zwischen Icon und Titel
+            title2.setSpacingBefore(Spacing1);
+            title3.setSpacingAfter(30);
+            document.add(title2);
+            document.add(title3);
 
             // Module-Anwendungsdaten
-            addTable(document, "Modulname", (moduleApplication.getName() != null) ? moduleApplication.getName() : "");
-            addTable(document, "Punkte", (moduleApplication.getPoints() != null) ? String.valueOf(moduleApplication.getPoints()) : "");
-            addTable(document, "Punktesystem", (moduleApplication.getPointSystem() != null) ? moduleApplication.getPointSystem() : "");
-            addTable(document, "Universität", (moduleApplication.getUniversity() != null) ? moduleApplication.getUniversity() : "");
+            addTable(document, "Punkte", (moduleApplication1.getPoints() != null) ? String.valueOf(moduleApplication1.getPoints()) : "");
+            addTable(document, "Punktesystem", (moduleApplication1.getPointSystem() != null) ? moduleApplication1.getPointSystem() : "");
+            addTable(document, "Universität", (moduleApplication1.getUniversity() != null) ? moduleApplication1.getUniversity() : "");
 
             //Studibüro daten
-            addTable(document, "Finale Entscheidung", (modulesConnection.getDecisionFinal() != null) ? String.valueOf(modulesConnection.getDecisionFinal()) : "");
-            addTable(document, "Kommentar zur Entscheidung", (modulesConnection.getCommentDecision() != null) ? modulesConnection.getCommentDecision() : "");
+            addTable(document, "Finale Entscheidung", (modulesConnection1.getDecisionFinal() != null) ? String.valueOf(modulesConnection1.getDecisionFinal()) : "");
+            addTable(document, "Kommentar zur Entscheidung", (modulesConnection1.getCommentDecision() != null) ? modulesConnection1.getCommentDecision() : "");
 
-            //Informationen Uni Leipzig
-            String addressText = "Universität Leipzig\n" +
-                    "Studienbüro Fakultät für Mathematik und Informatik\n" +
-                    "Neues Augusteum\n" +
-                    "04109 Leipzig\n\n" +
-                    "Leitung: Marco Neumann\n\n" +
-                    "Telefon: +49 341 97-32165\n" +
-                    "Telefax: +49 341 97-32193\n" +
-                    "Email-Adresse: studienbuero@math.uni-leipzig.de\n\n" +
-                    "Die aktuellen Sprechzeiten entnehmen\n" +
-                    "Sie bitte der Homepage:\n" +
-                    "www.mathcs.uni-leipzig.de/studium/studienbuero\n";
+            if (i + 1 < modulesConnections.size()) {
+                ModulesConnection modulesConnection2 = modulesConnections.get(i + 1);
+                ModuleApplication moduleApplication2 = modulesConnection2.getModuleApplication();
 
-            Font addressFont = getFont(HELVETICA, 10);
-            PdfPTable addressTable = new PdfPTable(1);
-            addressTable.setWidthPercentage(88);
-            addressTable.getDefaultCell().setBorder(Rectangle.NO_BORDER);
+                String titleText4 = "MODUL";
+                Font titleFont4 = getFont(HELVETICA_BOLD, 15);
+                Font titleFont5 = getFont(HELVETICA, 14);
+                Paragraph title4 = new Paragraph(titleText4, titleFont4);
+                Paragraph title5 = new Paragraph(moduleApplication2.getName().toUpperCase(), titleFont5);
+                title4.setAlignment(Element.ALIGN_LEFT);
+                title5.setAlignment(Element.ALIGN_LEFT);
+                title4.setIndentationLeft(30f);
+                title5.setIndentationLeft(30f);
+                float Spacing2 = 100f; // Abstand zwischen Icon und Titel
+                title4.setSpacingBefore(Spacing2);
+                title5.setSpacingAfter(50);
+                document.add(title4);
+                document.add(title5);
 
-            PdfPCell addressCell = new PdfPCell(new Paragraph(addressText, addressFont));
-            addressCell.setHorizontalAlignment(Element.ALIGN_LEFT);
-            addressCell.setBorder(Rectangle.NO_BORDER);
+                // Module-Anwendungsdaten
+                addTable(document, "Punkte", (moduleApplication2.getPoints() != null) ? String.valueOf(moduleApplication2.getPoints()) : "");
+                addTable(document, "Punktesystem", (moduleApplication2.getPointSystem() != null) ? moduleApplication2.getPointSystem() : "");
+                addTable(document, "Universität", (moduleApplication2.getUniversity() != null) ? moduleApplication2.getUniversity() : "");
 
-
-            addressTable.addCell(addressCell);
-            addressTable.setSpacingBefore(50f);
-            document.add(addressTable);
+                //Studibüro daten
+                addTable(document, "Finale Entscheidung", (modulesConnection2.getDecisionFinal() != null) ? String.valueOf(modulesConnection2.getDecisionFinal()) : "");
+                addTable(document, "Kommentar zur Entscheidung", (modulesConnection2.getCommentDecision() != null) ? modulesConnection2.getCommentDecision() : "");
+            }
         }
         document.close();
         return baos.toByteArray();
