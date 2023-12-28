f918bca48c78849d41d711287a390225b555cd69
Finn Kuschetzki <finnmail@icloud.com>
2023-12-28 21:52:03 +0100

    added ButtonLink component

---
+++ frontend/modulecrediting/src/components/ButtonLink.vue
@@ -0,0 +1,37 @@
+<!--
+button with link icon
+props:
+- route name to be opened on click
+slots:
+  - default slot for button name
+-->
+
+<script setup>
+import { useRouter } from "vue-router";
+
+const props = defineProps({
+  routeName: {
+    required: true,
+    type: String
+  }
+})
+
+const router = useRouter()
+
+const openLink = () => {
+  router.push({name: props.routeName})
+}
+</script>
+
+<template>
+  <div>
+    <Button @click="openLink">
+      <slot></slot>
+      <img src="../assets/icons/ArrowWhite.svg">
+    </Button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
