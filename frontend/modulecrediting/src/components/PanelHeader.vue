8284bc51ea31089dbe4904029a60640adaa95129
Finn Kuschetzki <finnmail@icloud.com>
2023-12-28 22:04:07 +0100

    changed PanelHeader to allow multiple externalModules and to separate modules with comma #114

--- frontend/modulecrediting/src/components/PanelHeader.vue
+++ frontend/modulecrediting/src/components/PanelHeader.vue
@@ -2,35 +2,36 @@
 import { defineProps } from 'vue';
 
 const props = defineProps({
-    moduleName: String,
-    internalModules: Array,
+    externalModules: {
+      required: true,
+      type: Array
+    },
+    internalModules: {
+      required: true,
+      type: Array
+    }
 });
 </script>
 
 <template>
-    <div class="panel-header">
-        <div class="module-name">
-            {{ moduleName }}
-        </div>
-
-        <div class="arrow-icon">
-                <img src="@/assets/icons/Arrow.svg" alt="Arrow Icon" />
-        </div>
-
-        <div class="internal-modules">
-            <p v-for="internalModule in internalModules" :key="internalModule.id">
-                {{ internalModule.moduleName }}
-            </p>
-        </div>
+  <div class="panel-header">
+    <div class="external-modules">
+      <p>{{ externalModules.join(', ') }}</p>
     </div>
+
+    <div class="arrow-icon">
+      <img src="@/assets/icons/ArrowRed.svg" alt="Arrow Icon" />
+    </div>
+
+    <div class="internal-modules">
+      <p>{{ internalModules.join(', ') }}</p>
+    </div>
+  </div>
 </template>
 
 <style scoped>
 .panel-header {
     display: flex;
     align-items: center;
-}
-.module-name {
-    margin-right: 10px;
 }
 </style>
