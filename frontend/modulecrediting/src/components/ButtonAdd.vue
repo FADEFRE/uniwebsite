74a11cde801494a1fc97ad6ee22c02f9c9b9a6fe
Finn Kuschetzki <finnmail@icloud.com>
2023-12-28 21:41:52 +0100

    added ButtonAdd component

---
+++ frontend/modulecrediting/src/components/ButtonAdd.vue
@@ -0,0 +1,22 @@
+<!--
+button with add icon
+slots:
+  - default slot for button name
+-->
+
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <Button>
+      <slot></slot>
+      <img src="../assets/icons/PlusIcon.svg">
+    </Button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
