79d3c20f2605ccb881b9f33167e373f528cfd5b6
Finn Kuschetzki <finnmail@icloud.com>
2024-01-03 20:43:43 +0100

    routing fix in HomepageView

--- frontend/modulecrediting/src/views/HomepageView.vue
+++ frontend/modulecrediting/src/views/HomepageView.vue
@@ -24,7 +24,7 @@ const openDetailView = () => {
 }
 
 const goToSubmitApplication = () => {
-    router.push({ name: 'SubmitApplication' })
+    router.push({ name: 'submitApplication' })
 }
 </script>
 
