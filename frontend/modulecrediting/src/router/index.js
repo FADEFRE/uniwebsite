dfb6555b6b5d0cdab900b5a9b240609e996f242d
Finn Kuschetzki <finnmail@icloud.com>
2024-01-05 12:53:40 +0100

    removed unused import in router/index.js

--- frontend/modulecrediting/src/router/index.js
+++ frontend/modulecrediting/src/router/index.js
@@ -8,7 +8,6 @@ import ChairmanDetailView from "@/views/ChairmanDetailView.vue";
 import LoginView from "@/views/LoginView.vue";
 import HomepageView from "@/views/HomepageView.vue"
 import ApplicationConfirmationView from "@/views/ApplicationConfirmationView.vue"
-import TestView from "@/views/TestView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
