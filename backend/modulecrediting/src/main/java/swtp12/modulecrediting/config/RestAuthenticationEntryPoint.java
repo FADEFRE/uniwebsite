96f8054662c21d384838800bee9da8903bd4d514
Frederik Kluge <fk13mami@studserv.uni-leipzig.de>
2024-01-10 21:56:05 +0100

    Security Config

---
+++ backend/modulecrediting/src/main/java/swtp12/modulecrediting/config/RestAuthenticationEntryPoint.java
@@ -0,0 +1,18 @@
+package swtp12.modulecrediting.config;
+
+import java.io.IOException;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class RestAuthenticationEntryPoint implements AuthenticationEntryPoint{
+    
+    @Override
+    public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException, ServletException {
+        httpServletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, e.getLocalizedMessage());
+    }
+}
