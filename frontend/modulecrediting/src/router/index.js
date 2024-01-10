488ef5c179464183c74967b77aa67df74192361f
Frederik Kluge <fk13mami@studserv.uni-leipzig.de>
2024-01-10 21:59:25 +0100

    changed routes to lazy loading

--- frontend/modulecrediting/src/router/index.js
+++ frontend/modulecrediting/src/router/index.js
@@ -1,13 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import SubmitApplicationView from '../views/SubmitApplicationView.vue'
-import StatusSearchView from "@/views/StatusSearchView.vue";
-import StatusDetailView from "@/views/StatusDetailView.vue";
-import SelectionView from "@/views/SelectionView.vue";
-import StudyOfficeDetailView from "@/views/StudyOfficeDetailView.vue";
-import ChairmanDetailView from "@/views/ChairmanDetailView.vue";
-import LoginView from "@/views/LoginView.vue";
-import JWTloginView from "@/views/JWTloginView.vue"
-import JWTstatusView from "@/views/JWTstatusView.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,66 +12,42 @@ const router = createRouter({
     {
       path: '/status',
       name: 'statusSearch',
-      component: StatusSearchView
+      component: () => import('../views/StatusDetailView.vue')
     },
     {
       path: '/status/:id',
       name: 'statusDetail',
-      component: StatusDetailView
+      component: () => import('../views/StatusDetailView.vue')
     },
     {
       path: '/login',
       name: 'login',
-      component: LoginView
+      component: () => import('../views/LoginView.vue')
     },
     {
       path: '/study-office',
       name: 'studyOfficeSelection',
-      component: SelectionView,
+      component: () => import('../views/SelectionView.vue'),
       meta: {forward: 'studyOfficeDetail'}
     },
     {
       path: '/study-office/:id',
       name: 'studyOfficeDetail',
-      component: StudyOfficeDetailView
+      component: () => import('../views/StudyOfficeDetailView.vue')
     },
     {
       path: '/chairman',
       name: 'chairmanSelection',
-      component: SelectionView,
+      component: () => import('../views/SelectionView.vue'),
       meta: {forward: 'chairmanDetail'}
     },
     {
       path: '/chairman/:id',
       name: 'chairmanDetail',
-      component: ChairmanDetailView
-    },
-    {
-      path: '/jwtlogin',
-      name: 'jwtlogin',
-      component: JWTloginView
-    },
-    {
-      path: '/jwtstatus',
-      name: 'jwtstatus',
-      component: JWTstatusView,
-      meta: {requiresAuth: true, allowedUsers: ['studienbüro', 'pav']}
+      component: () => import('../views/ChairmanDetailView.vue')
     }
   ]
 })
 
-router.beforeEach((to, from) => {
-  if (to.meta['requiresAuth']) {
-
-    // todo if logged in set username
-    // todo else set username to undefined
-    const username = 'studienbüro'
-
-    if (!to.meta['allowedUsers'].includes(username)) {
-      return {name: 'jwtlogin'}
-    }
-
-  }
-})
 
 export default router
