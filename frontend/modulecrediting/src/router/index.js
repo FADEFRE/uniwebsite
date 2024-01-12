e335be89658dd7cd759ff932a2e7240ff8ef0c84
Frederik Kluge <fk13mami@studserv.uni-leipzig.de>
2024-01-12 23:24:07 +0100

    added meta and Guards

--- frontend/modulecrediting/src/router/index.js
+++ frontend/modulecrediting/src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import SubmitApplicationView from '../views/SubmitApplicationView.vue'
+import { getAuthenticatedUser } from '../util/utils';
+import { useAuthStore } from '../store/authStore2';
+import httpResource from "../http/httpResource";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,47 +10,80 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: SubmitApplicationView
+      component: SubmitApplicationView,
+      meta: { authType: 'standard' }
     },
     {
       path: '/status',
       name: 'statusSearch',
-      component: () => import('../views/StatusSearchView.vue')
+      component: () => import('../views/StatusSearchView.vue'),
+      meta: { authType: 'standard' }
     },
     {
       path: '/status/:id',
       name: 'statusDetail',
-      component: () => import('../views/StatusDetailView.vue')
+      component: () => import('../views/StatusDetailView.vue'),
+      meta: { authType: 'standard' }
     },
     {
       path: '/login',
       name: 'login',
-      component: () => import('../views/LoginView.vue')
+      component: () => import('../views/LoginView.vue'),
+      meta: { authType: 'standard' }
     },
     {
       path: '/study-office',
       name: 'studyOfficeSelection',
       component: () => import('../views/SelectionView.vue'),
-      meta: {forward: 'studyOfficeDetail'}
+      meta: { authType: 'studyoffice', forward: 'studyOfficeDetail'}
     },
     {
       path: '/study-office/:id',
       name: 'studyOfficeDetail',
-      component: () => import('../views/StudyOfficeDetailView.vue')
+      component: () => import('../views/StudyOfficeDetailView.vue'),
+      meta: { authType: 'studyoffice'}
     },
     {
       path: '/chairman',
       name: 'chairmanSelection',
       component: () => import('../views/SelectionView.vue'),
-      meta: {forward: 'chairmanDetail'}
+      meta: { authType: 'chairman', forward: 'chairmanDetail'}
     },
     {
       path: '/chairman/:id',
       name: 'chairmanDetail',
-      component: () => import('../views/ChairmanDetailView.vue')
+      component: () => import('../views/ChairmanDetailView.vue'),
+      meta: { authType: 'chairman'}
     }
   ]
 })
 
+router.beforeEach(
+  async (to) => {
+    await getAuthenticatedUser();
+    const authUserStore = useAuthStore();
+    const id = authUserStore.getCurrentUserId;
+    const response = await httpResource.get(`/user/${id}/role`)
+    switch (to.meta.authType) {
+      case "standard":
+        return true;
+      case "studyoffice":
+        if (response.data === "ROLE_STUDY") { return true; }
+        return { name: 'login' };
+      case "chairman":
+        if (response.data === "ROLE_CHAIR") { return true; }
+        return { name: 'login' };
+      case "admin":
+        if (response.data === "ROLE_ADMIN") { return true; }
+        return { name: 'login' };
+      default:
+        break;
+    }
+
+    console.log("default");
+    return false;
+
+  }
+);
 
 export default router
