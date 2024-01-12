ca91c61f082d9082cb202d156e69590ce88a5da0
Frederik Kluge <fk13mami@studserv.uni-leipzig.de>
2024-01-12 23:25:00 +0100

    fail safe user id to -1

--- frontend/modulecrediting/src/store/authStore2.js
+++ frontend/modulecrediting/src/store/authStore2.js
@@ -5,18 +5,25 @@ export const useAuthStore = defineStore('authUser',{
     state: () => ({
         isAuthenticated: false,
         currentUser: {
-            userId: "",
+            userId: "-1",
             username: ""
         },
         intervalName: "" 
     }),
 
+    getters: {
+        getIsAuthenticated() { return this.isAuthenticated },
+        getCurrentUserId() { return this.currentUser.userId }
+    },
+
+
     actions: {
         setIsAuthenticated(value) { this.isAuthenticated = value; },
         setIntervalName(intervalName) {this.intervalName = intervalName; },
+        setCurrentUser(currentUser) { this.currentUser = currentUser},
         //async refresh() {},
         async logout() { 
-            this.userId = "";
+            this.userId = "-1";
             this.username = "";
             this.isAuthenticated = false }
     }
