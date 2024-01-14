1a87c5c6130cfce4df172d776e493227e7b1ab2c
Frederik Kluge <fk13mami@studserv.uni-leipzig.de>
2024-01-14 02:04:55 +0100

    added /api to every call #180

--- backend/modulecrediting/src/main/java/swtp12/modulecrediting/controller/ModulesConnectionController.java
+++ backend/modulecrediting/src/main/java/swtp12/modulecrediting/controller/ModulesConnectionController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @CrossOrigin
-@RequestMapping("/modules-connection")
+@RequestMapping("/api/modules-connection")
 public class ModulesConnectionController {
     @Autowired
     ModulesConnectionService modulesConnectionService;
