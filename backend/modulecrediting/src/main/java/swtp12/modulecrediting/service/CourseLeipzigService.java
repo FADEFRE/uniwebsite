531233dbdefb605d39ddbc43ccd6d52d984b2a4c
Frederik Kluge <fk13mami@studserv.uni-leipzig.de>
2024-01-17 02:10:54 +0100

    added methods to delete/deactivate and check the isActive state #142

--- backend/modulecrediting/src/main/java/swtp12/modulecrediting/service/CourseLeipzigService.java
+++ backend/modulecrediting/src/main/java/swtp12/modulecrediting/service/CourseLeipzigService.java
@@ -8,8 +8,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import swtp12.modulecrediting.model.Application;
 import swtp12.modulecrediting.model.CourseLeipzig;
+import swtp12.modulecrediting.model.ModuleLeipzig;
+import swtp12.modulecrediting.model.ModulesConnection;
 import swtp12.modulecrediting.repository.CourseLeipzigRepository;
+import swtp12.modulecrediting.repository.ModulesConnectionRepository;
 
 
 
@@ -17,6 +21,9 @@ import swtp12.modulecrediting.repository.CourseLeipzigRepository;
 public class CourseLeipzigService {
     @Autowired
     CourseLeipzigRepository courseLeipzigRepository;
+    
+    @Autowired
+    private ModulesConnectionRepository modulesConnectionRepository;
 
     public CourseLeipzig getCourseLeipzigByName(String name) {
         Optional<CourseLeipzig> courseLeipzig = courseLeipzigRepository.findByName(name);
@@ -29,5 +36,43 @@ public class CourseLeipzigService {
 
     public List<CourseLeipzig> getAllCoursesLeipzig() {
         return courseLeipzigRepository.findAll();
+    }
+
+    public CourseLeipzig getCourseLeipzigById(String id) {
+        Optional<CourseLeipzig> courseLeipzig = courseLeipzigRepository.findById(id);
+        if(courseLeipzig.isPresent()) 
+            return courseLeipzig.get();
+
+        throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Course Leipzig not found with given id: " + id);
+    }
+
+    public Boolean deleteCourseLeipzig(String id) {
+        CourseLeipzig courseLeipzig = getCourseLeipzigById(id);
+        if (!courseLeipzig.getIsActive())
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Module Leipzig is already deactivated with id: " + id);
+        courseLeipzig.setIsActive(false);
+        if (checkIfDeletionIsPossible(courseLeipzig)) {
+            courseLeipzigRepository.deleteById(id);
+            return true;
+        }
+        else return false;
+    }
+
+    private Boolean checkIfDeletionIsPossible(CourseLeipzig courseLeipzig) {
+        List<ModulesConnection> allModulesConnections = modulesConnectionRepository.findAll();
+        if (allModulesConnections.isEmpty()) throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "There are no connections in the database");
+        Boolean check = false;
+        for (ModulesConnection modulesConnection : allModulesConnections) {
+            Application application = modulesConnection.getApplication();
+            CourseLeipzig applicationCourseLeipzig = application.getCourseLeipzig();
+            if (!applicationCourseLeipzig.equals(courseLeipzig)) { check = true; }
+            else check = false;
+        }
+        return check;
+    }
+
+    public Boolean getCourseLeipzigState(String id) {
+        CourseLeipzig courseLeipzig = getCourseLeipzigById(id);
+        return courseLeipzig.getIsActive();
     }
 }
