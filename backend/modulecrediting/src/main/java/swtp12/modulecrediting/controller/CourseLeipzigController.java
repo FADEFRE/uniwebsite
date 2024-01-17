8f05807b9af8b0d4ab1494d15ef081a17b8389f9
Frederik Kluge <fk13mami@studserv.uni-leipzig.de>
2024-01-17 02:11:21 +0100

    added api routing to delete/deactivate Courses and Modules #138

--- backend/modulecrediting/src/main/java/swtp12/modulecrediting/controller/CourseLeipzigController.java
+++ backend/modulecrediting/src/main/java/swtp12/modulecrediting/controller/CourseLeipzigController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,11 +22,32 @@ import swtp12.modulecrediting.service.CourseLeipzigService;
 @RequestMapping("/api/courses-leipzig")
 public class CourseLeipzigController {
     @Autowired
-    CourseLeipzigService service;
+    CourseLeipzigService courseLeipzigService;
 
     @GetMapping
     @JsonView(Views.coursesWithModules.class)
     ResponseEntity<List<CourseLeipzig>> getCourseLeipzigs() {
-        return ResponseEntity.ok(service.getAllCoursesLeipzig());
+        return ResponseEntity.ok(courseLeipzigService.getAllCoursesLeipzig());
     }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CourseLeipzig> getSingleCourseLeipzig(@PathVariable String id) {
+        return ResponseEntity.ok(courseLeipzigService.getCourseLeipzigById(id));
+    }
+
+
+    @DeleteMapping("/{id}/delete")
+    public ResponseEntity<String> deleteCourseLeipzig(@PathVariable String id) {
+        if (courseLeipzigService.deleteCourseLeipzig(id)) {
+            return ResponseEntity.ok("DELETED");
+        }
+        else return ResponseEntity.ok("DEACTIVATED");
+    }
+
+    @GetMapping("/{id}/state")
+    public ResponseEntity<Boolean> getCourseLeipzigState(@PathVariable String id) {
+        return ResponseEntity.ok(courseLeipzigService.getCourseLeipzigState(id));
+    }
+    
 }
--- backend/modulecrediting/src/main/java/swtp12/modulecrediting/controller/ModuleLeipzigController.java
+++ backend/modulecrediting/src/main/java/swtp12/modulecrediting/controller/ModuleLeipzigController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,18 +15,42 @@ import com.fasterxml.jackson.annotation.JsonView;
 
 import swtp12.modulecrediting.model.ModuleLeipzig;
 import swtp12.modulecrediting.model.Views;
-import swtp12.modulecrediting.repository.ModuleLeipzigRepository;
+import swtp12.modulecrediting.service.ModuleLeipzigService;
+
 
 @RestController
 @RequestMapping("/api/modules-leipzig")
 @CrossOrigin
 public class ModuleLeipzigController {
+
     @Autowired
-    private ModuleLeipzigRepository modulLeipzigRepository;
+    private ModuleLeipzigService moduleLeipzigService;
 
     @GetMapping
     @JsonView(Views.modulesWithoutCourse.class)
-    ResponseEntity<List<ModuleLeipzig>> getModulesLeipzig() {
-        return ResponseEntity.ok(modulLeipzigRepository.findAll());
+    ResponseEntity<List<ModuleLeipzig>> getAllModulesLeipzig() {
+        return ResponseEntity.ok(moduleLeipzigService.getAllModulesLeipzig());
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ModuleLeipzig> getSingleModulesLeipzig(@PathVariable String id) {
+        return ResponseEntity.ok(moduleLeipzigService.getModuleLeipzigById(id));
+    }
+
+
+    @DeleteMapping("/{id}/delete")
+    public ResponseEntity<String> deleteModulesLeipzig(@PathVariable String id) {
+        if (moduleLeipzigService.deleteModulesLeipzig(id)) {
+            return ResponseEntity.ok("DELETED");
+        }
+        else return ResponseEntity.ok("DEACTIVATED");
+    }
+
+    @GetMapping("/{id}/state")
+    public ResponseEntity<Boolean> getModuleLeipzigState(@PathVariable String id) {
+        return ResponseEntity.ok(moduleLeipzigService.getModuleLeipzigState(id));
+    }
+    
+
+
 }
