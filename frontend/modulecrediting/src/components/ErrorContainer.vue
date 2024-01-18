665ec7f88933b6f72028be8466c9c953cb9fbeea
Toan Anh Nguyen <toananh988@gmail.com>
2024-01-18 18:28:26 +0100

    created Error page, no routing, styling maybe need some adjustment

---
+++ frontend/modulecrediting/src/components/ErrorContainer.vue
@@ -0,0 +1,47 @@
+<script setup>
+import { useRouter } from 'vue-router';
+import ButtonLink from '../components/ButtonLink.vue';
+
+const router = useRouter();
+const redirectToHome = () => {
+    router.push({ name: 'home' });
+};
+</script>
+
+<template>
+    <div class="error-container">
+        <div>
+            <h2>Fehler 404: Seite nicht gefunden</h2>
+            <p>Leider kann die gewünschte Seite nicht angezeigt werden.</p>
+        </div>
+        <div class="button-container">
+            <ButtonLink @click="redirectToHome">Zur Startseite</ButtonLink>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.error-container {
+    background-color: white;
+    display: flex;
+    padding: 0.9375rem 1.25rem;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 1.25rem;
+    align-self: stretch;
+    max-width: 27.42188rem;
+    max-height: 11.25rem;
+    margin: 5rem auto 0;
+
+}
+
+.button-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 1.25rem;
+}
+</style>
+
+
---
+++ frontend/modulecrediting/src/views/ErrorView.vue
@@ -0,0 +1,14 @@
+
+<script setup>
+import ErrorContainer from '../components/ErrorContainer.vue'
+</script>
+
+<template>
+     <div class="main">
+        <ErrorContainer/>
+    </div>
+</template>
+
+<style>
+
+</style>
