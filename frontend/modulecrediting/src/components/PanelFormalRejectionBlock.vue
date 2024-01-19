4b704d58326eee717597bb826faa8389a4491daf
Finn Kuschetzki <finnmail@icloud.com>
2024-01-20 00:20:14 +0100

    added PanelFormalRejectionBlock #237

---
+++ frontend/modulecrediting/src/components/PanelFormalRejectionBlock.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref } from "vue";
+
+const props = defineProps({
+  type: {
+    required: true,
+    type: String,
+    validator(value) {
+      return ['edit', 'readonly'].includes(value)
+    }
+  },
+  comment: {
+    type: String
+  }
+})
+
+const comment = ref(props.comment)
+
+defineExpose({
+  comment
+})
+</script>
+
+<template>
+  <div>
+    <p>Formfehler</p>
+    <textarea :readonly="type === 'readonly'" v-model="comment"></textarea>
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
