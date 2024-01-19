1779142875517bbef0410bd191a3ead4ae6d3496
Frederik Kluge <fk13mami@studserv.uni-leipzig.de>
2024-01-19 01:02:02 +0100

    updated

--- backend/modulecrediting/src/main/java/swtp12/modulecrediting/dto/ModuleLeipzigCreateDTO.java
+++ backend/modulecrediting/src/main/java/swtp12/modulecrediting/dto/ModuleLeipzigCreateDTO.java
@@ -1,7 +1,5 @@
 package swtp12.modulecrediting.dto;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,10 +7,8 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
-// used to create a new module leipzig !! different to all the other create & update dtos. they are used for creating,updating an application
 public class ModuleLeipzigCreateDTO {
     private String name;
 
-    @JsonIgnore
     private String code;
 }
