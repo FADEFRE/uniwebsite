1bf650624136669ec1271c665b401d4ae0329992
Luca Kippe <ny63fabi@studserv.uni-leipzig.de>
2024-01-25 22:29:16 +0100

    added LoadingContainer

---
+++ frontend/modulecrediting/src/components/LoadingContainer.vue
@@ -0,0 +1,39 @@
+<script setup>
+</script>
+
+<template>
+    <div class="loading-container">
+        <div class="loader"></div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+@import '../assets/variables.scss';
+@import '../assets/mixins.scss';
+
+.loading-container {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%);
+}
+
+.loader {
+    border: 5px solid rgba($color: $dark-gray, $alpha: 0.1);
+    border-top: 5px solid $red;
+    border-radius: 50%;
+    width: 50px;
+    height: 50px;
+    animation: spin 2s linear infinite;
+
+}
+
+@keyframes spin {
+    0% {
+        transform: rotate(0deg);
+    }
+
+    100% {
+        transform: rotate(360deg);
+    }
+}</style>
