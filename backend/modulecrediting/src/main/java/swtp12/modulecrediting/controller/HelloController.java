2fbf65f9c06dd7ca1be70038d4deec46f1d7ff62
Frederik Kluge <fk13mami@studserv.uni-leipzig.de>
2024-01-28 02:28:35 +0100

    added debug controller

---
+++ backend/modulecrediting/src/main/java/swtp12/modulecrediting/controller/HelloController.java
@@ -0,0 +1,20 @@
+package swtp12.modulecrediting.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+@RequestMapping("/")
+@CrossOrigin
+public class HelloController {
+
+    @GetMapping
+    public String hello() {
+        return "hello";
+    }
+    
+    
+}
