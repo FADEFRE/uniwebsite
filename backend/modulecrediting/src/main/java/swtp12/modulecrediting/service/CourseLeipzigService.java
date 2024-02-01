c26b12f2337453ec6f9babe0c8d02d235a3ef94b
Frederik Kluge <fk13mami@studserv.uni-leipzig.de>
2024-02-01 14:08:07 +0100

    removed imports

--- backend/modulecrediting/src/main/java/swtp12/modulecrediting/service/CourseLeipzigService.java
+++ backend/modulecrediting/src/main/java/swtp12/modulecrediting/service/CourseLeipzigService.java
@@ -1,11 +1,9 @@
 package swtp12.modulecrediting.service;
 
-import java.nio.charset.CoderResult;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
