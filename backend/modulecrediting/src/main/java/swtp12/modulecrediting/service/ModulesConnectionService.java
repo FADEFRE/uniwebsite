f5ee6b6aae4832e00c2fdedbf04681e89d13e7a3
Luca Kippe <ny63fabi@studserv.uni-leipzig.de>
2024-02-06 09:08:42 +0100

    only applications with status==Abgeschlossen #295

--- backend/modulecrediting/src/main/java/swtp12/modulecrediting/service/ModulesConnectionService.java
+++ backend/modulecrediting/src/main/java/swtp12/modulecrediting/service/ModulesConnectionService.java
@@ -8,6 +8,7 @@ import org.springframework.web.server.ResponseStatusException;
 import swtp12.modulecrediting.dto.ExternalModuleDTO;
 import swtp12.modulecrediting.dto.ModuleLeipzigDTO;
 import swtp12.modulecrediting.dto.ModulesConnectionDTO;
+import swtp12.modulecrediting.model.EnumApplicationStatus;
 import swtp12.modulecrediting.model.ExternalModule;
 import swtp12.modulecrediting.model.ModuleLeipzig;
 import swtp12.modulecrediting.model.ModulesConnection;
@@ -217,6 +218,8 @@ public class ModulesConnectionService {
 
             // skip original modules connections
             if(m.getModulesConnectionOriginal() == null) continue;
+
+            if(m.getApplication().getFullStatus() != EnumApplicationStatus.ABGESCHLOSSEN) continue;
 
             if(m.getDecisionFinal() == unedited || m.getDecisionFinal() == asExamCertificate) continue;
 
