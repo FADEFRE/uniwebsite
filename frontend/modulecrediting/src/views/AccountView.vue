3b1cca230b97605f36c6d5fbad20c1a0a390ae8e
Finn Kuschetzki <finnmail@icloud.com>
2024-02-07 13:41:21 +0100

    created AccountView and AccountViewAdminChild as dummy and implemented routing #324 #325

---
+++ frontend/modulecrediting/src/views/AccountView.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    Account View
+    <router-view />
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
---
+++ frontend/modulecrediting/src/views/AccountViewAdminChild.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    Admin Child
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
