ba17331804596de48860100dc53235f6ee1a5334
Finn Kuschetzki <finnmail@icloud.com>
2024-02-16 01:45:17 +0100

    implemented AccountSettings in AccountView #324

--- frontend/modulecrediting/src/views/AccountView.vue
+++ frontend/modulecrediting/src/views/AccountView.vue
@@ -1,11 +1,11 @@
 <script setup>
-
+import AccountSettings from "../components/AccountSettings.vue";
 </script>
 
 <template>
   <div class="main">
     <div class="content-container">
-      Account View
+      <AccountSettings />
       <router-view />
     </div>
   </div>
