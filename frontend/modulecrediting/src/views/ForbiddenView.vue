1268a2649b0b669794d6aa3f67778d43f406cde3
Finn Kuschetzki <finnmail@icloud.com>
2024-02-20 22:11:06 +0100

    cleanup ForbiddenView

--- frontend/modulecrediting/src/views/ForbiddenView.vue
+++ frontend/modulecrediting/src/views/ForbiddenView.vue
@@ -1,20 +1,13 @@
 <script setup>
 import ErrorContainer from '../components/ErrorContainer.vue';
-import { useRoute, useRouter } from 'vue-router';
-import ButtonLink from "@/components/ButtonLink.vue";
-
-const router = useRouter();
-const props = defineProps(['customTitle', 'customMessage']);
-
-
-
-
 </script>
 
 <template>
     <div class="main centered">
-        <ErrorContainer :customTitle="'403 - Forbidden'"
-            :customMessage="'Du darfst nicht auf diese Adresse zugreifen'" />
+        <ErrorContainer
+            customTitle="403 - Forbidden"
+            customMessage="Du darfst nicht auf diese Adresse zugreifen"
+        />
     </div>
 </template>
 
