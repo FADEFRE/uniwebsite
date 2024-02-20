4c993407bbd8ee39f55e27a576d28fefc1d25887
Finn Kuschetzki <finnmail@icloud.com>
2024-02-20 15:00:42 +0100

    cleanup in ApplicationConfirmationView

--- frontend/modulecrediting/src/views/ApplicationConfirmationView.vue
+++ frontend/modulecrediting/src/views/ApplicationConfirmationView.vue
@@ -1,59 +1,41 @@
 <script setup>
 import { useRoute, useRouter } from 'vue-router';
-import { ref, onBeforeMount } from 'vue';
-import { getApplicationByIdForStatus } from '@/scripts/axios-requests';
-import { url } from '@/scripts/url-config';
-import httpResource from "@/scripts/httpResource";
+import { onBeforeMount } from 'vue';
 import ConfirmationContainer from '../components/ConfirmationContainer.vue';
 import ButtonLink from "@/components/ButtonLink.vue";
 import ButtonDownload from '../components/ButtonDownload.vue';
+import { url } from '@/scripts/url-config';
 
-const route = useRoute();
-const router = useRouter();
-const isInvalid = ref(false);
-const applicationData = ref(undefined);
-let id = undefined;
-let pdfDataLink = undefined;
+const route = useRoute()
+const router = useRouter()
+let id = undefined
+let pdfDataLink = undefined
 
 onBeforeMount(() => {
-    id = route.params.id;
-    pdfDataLink = `${url}/file/pdf-documents/application/${id}`;
-    getApplicationByIdForStatus(id)
-        .then(data => applicationData.value = data)
-        .catch(error => {
-            console.log(error);
-            applicationData.value = 'error';
-        });
-});
+  id = route.params.id;
+  pdfDataLink = `${url}/file/pdf-documents/application/${id}`
+})
 
 const openDetailView = () => {
-    httpResource.get(`${url}/api/applications/${id}/exists`)
-        .then(response => {
-            if (response.data) {
-                const routeData = router.resolve({ name: 'statusDetail', params: { id: id } });
-                window.open(routeData.href, '_top');
-            } else {
-                isInvalid.value = true;
-            }
-        });
-};
+  router.push({ name: 'statusDetail', params: { id: id } })
+}
 
 const openPdf = () => {
-    window.open(pdfDataLink, '_blank');
-};
+  window.open(pdfDataLink, '_blank');
+}
 
 
 </script>
 
 <template>
-    <div class="main centered" v-if="applicationData">
-        <ConfirmationContainer :id="id">
-            <ButtonDownload @click="openPdf">
-              Antrag herunterladen
-            </ButtonDownload>
-            <ButtonLink @click="openDetailView" class="status-button">Status einsehen</ButtonLink>
-        </ConfirmationContainer>
-    </div>
+  <div class="main centered">
+    <ConfirmationContainer :id="id">
+      <ButtonDownload @click="openPdf">
+        Antrag herunterladen
+      </ButtonDownload>
+      <ButtonLink @click="openDetailView" class="status-button">Status einsehen</ButtonLink>
+    </ConfirmationContainer>
+  </div>
 </template>
 
 <style scoped lang="scss">
