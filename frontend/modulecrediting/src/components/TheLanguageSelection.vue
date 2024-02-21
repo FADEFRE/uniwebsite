8b0ffb1eef8714f0f517e36073935d4ea0d23c80
Finn Kuschetzki <finnmail@icloud.com>
2024-02-21 15:12:42 +0100

    cleanup in TheLanguageSelection

--- frontend/modulecrediting/src/components/TheLanguageSelection.vue
+++ frontend/modulecrediting/src/components/TheLanguageSelection.vue
@@ -13,10 +13,10 @@ watch(useRoute(), newRoute => {
 </script>
 
 <template>
-    <div v-if="showLanguage" class="language-selection-container">
-        <Button @click="translate.switchLanguage('de')" class="language-button" :class="{ 'active': store.locale == 'de' }">DE</Button>
-        <Button @click="translate.switchLanguage('en')" class="language-button" :class="{ 'active': store.locale == 'en' }">EN</Button>
-    </div>
+  <div v-if="showLanguage" class="language-selection-container">
+    <Button @click="translate.switchLanguage('de')" class="language-button" :class="{ 'active': store.locale == 'de' }">DE</Button>
+    <Button @click="translate.switchLanguage('en')" class="language-button" :class="{ 'active': store.locale == 'en' }">EN</Button>
+  </div>
 </template>
 
 <style scoped lang="scss">
@@ -24,34 +24,34 @@ watch(useRoute(), newRoute => {
 @use '@/assets/styles/global' as *;
 
 .language-selection-container {
-    width: fit-content;
-    display: flex;
+  width: fit-content;
+  display: flex;
 }
 .language-button {
+  background-color: transparent;
+  padding: 0 spacing(s);
+
+  color: $white;
+  font-family: "Jost";
+  font-size: 1rem;
+  font-weight: 400;
+  line-height: spacing(m);
+
+  &:first-child {
+    border-right: 1px solid $white;
+  }
+
+  &:hover {
     background-color: transparent;
-    padding: 0 spacing(s);
-
-    color: $white;
-    font-family: "Jost";
-    font-size: 1rem;
-    font-weight: 400;
-    line-height: spacing(m);
-
-    &:first-child {
-        border-right: 1px solid $white;
-    }
-
-    &:hover {
-        background-color: transparent;
-        text-decoration: underline;
-    }
+    text-decoration: underline;
+  }
 }
 
 
 .active {
-    font-weight: 550;
-    &:hover {
-        text-decoration: none;
-    }
+  font-weight: 550;
+  &:hover {
+    text-decoration: none;
+  }
 }
 </style>
