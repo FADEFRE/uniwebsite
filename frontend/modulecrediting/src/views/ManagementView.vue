1bcbc666bc943f5f13fcb62c98092cf7a8137c85
Luca Kippe <ny63fabi@studserv.uni-leipzig.de>
2024-02-22 23:15:38 +0100

    #347 added seperator in managementview

--- frontend/modulecrediting/src/views/ManagementView.vue
+++ frontend/modulecrediting/src/views/ManagementView.vue
@@ -1,26 +1,25 @@
 <script setup>
-import ManagementCreateContainer from '../components/ManagementCreate.vue';
-import ManagementEditCourse from '../components/ManagementEditCourse.vue';
-import ManagementViewCourse from '../components/ManagementViewCourse.vue';
-import ManagementViewModule from '../components/ManagementViewModule.vue';
-import ManagementViewAdminChild from "./ManagementViewAdminChild.vue";
+import ManagementCreateContainer from '@/components/ManagementCreate.vue';
+import ManagementEditCourse from '@/components/ManagementEditCourse.vue';
+import ManagementViewCourse from '@/components/ManagementViewCourse.vue';
+import ManagementViewModule from '@/components/ManagementViewModule.vue';
 </script>
 
 
 <template>
   <div class="main">
     <div class="create-container-list">
+      <router-view />
 
       <ManagementCreateContainer type="course" />
       <ManagementCreateContainer type="module" />
       <ManagementEditCourse />
+      <div class="break-container">
+              <div class="break-line"></div>
+              <h4 class="break-text">ÜBERSICHT</h4>
+        </div>
       <ManagementViewCourse />
       <ManagementViewModule />
-
-      <!-- todo add separator -->
-
-      <router-view />
-
     </div>
   </div>
 </template>
@@ -34,5 +33,28 @@ import ManagementViewAdminChild from "./ManagementViewAdminChild.vue";
 .create-container-list {
   @include verticalList(l);
   width: 100%;
+}
+
+
+.break-container {
+  width: 100%;
+  margin: spacing(m) 0;
+  padding: 0 spacing(m);
+  position: relative;
+}
+.break-line {
+  width: 100%;
+  height: 2px;
+  background-color: $dark-gray;
+}
+
+.break-text {
+  width: 7rem;
+  text-align: center;
+  background-color: $gray;
+  position: absolute;
+  left: 50%;
+  top: -0.65rem;
+  transform: translateX(-50%);
 }
 </style>
