999156ef3c73f966222453dda3839d374aae9198
Finn Kuschetzki <finnmail@icloud.com>
2024-02-25 19:32:06 +0100

    implemented keyboard controls on EditIcon #406

--- frontend/modulecrediting/src/assets/icons/EditIcon.vue
+++ frontend/modulecrediting/src/assets/icons/EditIcon.vue
@@ -1,10 +1,13 @@
-<template>
-    <div class="edit-container">
-        <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M8.5 4H1V19H16V11.5M7.5 12.5L8 10L16 2L18 4L10 12L7.5 12.5Z" stroke="#262A31" stroke-width="2"/>
-</svg>
+<script setup>
+ const emit = defineEmits(['click'])
+</script>
 
-    </div>
+<template>
+  <div tabindex="0" @keydown.enter.prevent="emit('click')" @click="emit('click')" class="edit-container">
+    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M8.5 4H1V19H16V11.5M7.5 12.5L8 10L16 2L18 4L10 12L7.5 12.5Z" stroke="#262A31" stroke-width="2"/>
+    </svg>
+  </div>
 </template>
 
 <style scoped lang="scss">
