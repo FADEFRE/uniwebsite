79fde6bea5407346c8409ff349b9197a1821f5f7
Finn Kuschetzki <finnmail@icloud.com>
2024-02-25 20:03:47 +0100

    added CustomDropdown

---
+++ frontend/modulecrediting/src/components/CustomDropdown.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from "vue";
+import ArrowIcon from "@/assets/icons/ArrowIcon.vue";
+
+const dropdown = ref()
+</script>
+
+<template>
+  <Dropdown ref="dropdown" @focus="dropdown.show()">
+    <template #filtericon>
+      <img class="search-icon" src="@/assets/icons/SearchIcon.svg">
+    </template>
+    <template #dropdownicon>
+      <ArrowIcon color="white" direction="down" />
+    </template>
+  </Dropdown>
+</template>
+
+<style scoped lang="scss">
+.search-icon {
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  right: spacing(s);
+}
+</style>
