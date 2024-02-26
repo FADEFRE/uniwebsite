8d64a5e1fbbd6acf205a427e6784f71b65d9b53e
Finn Kuschetzki <finnmail@icloud.com>
2024-02-26 13:43:41 +0100

    fixed 409 catch and added empty checks in ManagementCreate

--- frontend/modulecrediting/src/components/ManagementCreate.vue
+++ frontend/modulecrediting/src/components/ManagementCreate.vue
@@ -14,57 +14,75 @@ const coursename = ref('');
 const modulename = ref('');
 const modulecode = ref('');
 
+const courseNameEmpty = ref(false);
 const courseExists = ref(false);
+const moduleNameEmpty = ref(false);
 const moduleExists = ref(false);
 
 const createCourseLeipzig = () => {
-    postCourseLeipzig(coursename.value)
-        .then(_ => location.reload())
-        .catch(error => {
-            if (error.response.status) {  // todo check for specific error
-                courseExists.value = true
-            } else {
-                location.reload()
-            }
-        })
-}
-const createModuleLeipzig = () => {
-    postModuleLeipzig(modulename.value, modulecode.value)
-        .then(_ => location.reload())
-        .catch(error => {
-            if (error.response.status) {  // todo check for specific error
-                moduleExists.value = true
-            } else {
-                location.reload()
-            }
-        })
+  courseNameEmpty.value = false
+  courseExists.value = false
+  if (!coursename.value) {
+    courseNameEmpty.value = true
+    return
+  }
+  postCourseLeipzig(coursename.value)
+      .then(_ => location.reload())
+      .catch(error => {
+        if (error.response.status === 409) {  // todo check for specific error
+          courseExists.value = true
+        } else {
+          location.reload()
+        }
+      })
 }
 
+const createModuleLeipzig = () => {
+  moduleNameEmpty.value = false
+  moduleExists.value = false
+  if (!modulename.value) {
+    moduleNameEmpty.value = true
+    return
+  }
+  postModuleLeipzig(modulename.value, modulecode.value)
+      .then(_ => location.reload())
+      .catch(error => {
+        if (error.response.status === 409) {  // todo check for specific error
+          moduleExists.value = true
+        } else {
+          location.reload()
+        }
+      })
+}
 </script>
 
 <template>
-    <div v-if="type === 'course'" class="management-create-container">
-        <h2>Studiengang erstellen</h2>
-        <div class="input-container">
-            <InputText type="text" class="white" placeholder="Name des Studiengangs" v-model="coursename"
-                :class="{ 'invalid': courseExists }" />
-            <small v-if="courseExists" class="invalid-text">Studiengang existiert bereits</small>
-        </div>
-        <ButtonLink @click="createCourseLeipzig">Studiengang hinzufügen</ButtonLink>
+  <div v-if="type === 'course'" class="management-create-container">
+    <h2>Studiengang erstellen</h2>
+    <div class="input-container">
+      <InputText type="text" placeholder="Name des Studiengangs" v-model="coursename"
+                 class="white" :class="{ 'invalid': courseNameEmpty || courseExists }" />
+      <small v-if="courseNameEmpty" class="invalid-text">Name des Studiengangs darf nicht leer sein</small>
+      <small v-if="courseExists" class="invalid-text">Studiengang existiert bereits</small>
     </div>
+    <ButtonLink @click="createCourseLeipzig">Studiengang hinzufügen</ButtonLink>
+  </div>
 
 
-    <div v-else-if="type === 'module'" class="management-create-container">
-        <h2>Modul erstellen</h2>
-        <div class="input-container">
-            <div class="module-input-container">
-                <InputText type="text" class="white" placeholder="Modulname" v-model="modulename" :class="{ 'invalid': moduleExists }" />
-                <InputText type="text" class="white" placeholder="Modulcode" v-model="modulecode" :class="{ 'invalid': moduleExists }" />
-            </div>
-            <small v-if="moduleExists" class="invalid-text">Studiengang existiert bereits</small>
-        </div>
-        <ButtonLink @click="createModuleLeipzig">Modul hinzufügen</ButtonLink>
+  <div v-else-if="type === 'module'" class="management-create-container">
+    <h2>Modul erstellen</h2>
+    <div class="input-container">
+      <div class="module-input-container">
+        <InputText type="text" placeholder="Modulname" v-model="modulename"
+                   class="white" :class="{ 'invalid': moduleNameEmpty || moduleExists }" />
+        <InputText type="text"  placeholder="Modulcode" v-model="modulecode"
+                   class="white" :class="{ 'invalid': moduleExists }" />
+      </div>
+      <small v-if="moduleNameEmpty" class="invalid-text">Modulname darf nicht leer sein</small>
+      <small v-if="moduleExists" class="invalid-text">Modulname oder Modulcode existiert bereits</small>
     </div>
+    <ButtonLink @click="createModuleLeipzig">Modul hinzufügen</ButtonLink>
+  </div>
 </template>
 
 
