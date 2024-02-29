806fcaba9e543f1a1bd3fa9a2769b0272d96a5a8
Frederik Kluge <fk13mami@studserv.uni-leipzig.de>
2024-02-29 13:36:35 +0100

    organized imports

--- backend/modulecrediting/src/main/java/swtp12/modulecrediting/service/ExternalModuleService.java
+++ backend/modulecrediting/src/main/java/swtp12/modulecrediting/service/ExternalModuleService.java
@@ -1,17 +1,19 @@
 package swtp12.modulecrediting.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
+
 import swtp12.modulecrediting.dto.ExternalModuleDTO;
 import swtp12.modulecrediting.model.ExternalModule;
 import swtp12.modulecrediting.model.ModulesConnection;
 import swtp12.modulecrediting.model.PdfDocument;
 import swtp12.modulecrediting.repository.ExternalModuleRepository;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
 
 @Service
 public class ExternalModuleService {
