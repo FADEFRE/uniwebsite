4e4c7c1bf8f171be9585c57dfb273a6b7441903d
Frederik Kluge <fk13mami@studserv.uni-leipzig.de>
2024-02-29 12:52:22 +0100

    fixed depreciation issue

--- backend/modulecrediting/src/test/java/swtp12/modulecrediting/service/ModulesConnectionServiceTest.java
+++ backend/modulecrediting/src/test/java/swtp12/modulecrediting/service/ModulesConnectionServiceTest.java
@@ -34,7 +34,7 @@ public class ModulesConnectionServiceTest {
 
     @BeforeEach
     public void setUp() {
-        MockitoAnnotations.initMocks(this);
+        MockitoAnnotations.openMocks(this);
     }
 
     @Test
