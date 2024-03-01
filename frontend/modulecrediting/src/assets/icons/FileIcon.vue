87ad683b9ba18135bb5d27e560b2db5aab05504a
Finn Kuschetzki <finnmail@icloud.com>
2024-03-01 13:52:54 +0100

    set aria-hidden in icons #439

--- frontend/modulecrediting/src/assets/icons/FileIcon.vue
+++ frontend/modulecrediting/src/assets/icons/FileIcon.vue
@@ -8,19 +8,17 @@ const props = defineProps({
 </script>
 
 <template>
-    <svg v-if="type==='upload'" class="file-icon" viewBox="0 0 22 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M6 7L11 2L16 7" stroke="#D83D3B" stroke-width="2.5"/>
-<path d="M11 13V3" stroke="#D83D3B" stroke-width="2.5"/>
-<path d="M2 11V18H20V11" stroke="#D83D3B" stroke-width="2.5"/>
-</svg>
+  <svg v-if="type==='upload'" class="file-icon" viewBox="0 0 22 20" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+    <path d="M6 7L11 2L16 7" stroke="#D83D3B" stroke-width="2.5"/>
+    <path d="M11 13V3" stroke="#D83D3B" stroke-width="2.5"/>
+    <path d="M2 11V18H20V11" stroke="#D83D3B" stroke-width="2.5"/>
+  </svg>
 
-<svg v-else class="file-icon" viewBox="0 0 22 19" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M16 6L11 11L6 6" stroke="#D83D3B" stroke-width="2.5"/>
-<path d="M11 10V0" stroke="#D83D3B" stroke-width="2.5"/>
-<path d="M2 10V17H20V10" stroke="#D83D3B" stroke-width="2.5"/>
-</svg>
-
-
+  <svg v-else class="file-icon" viewBox="0 0 22 19" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+    <path d="M16 6L11 11L6 6" stroke="#D83D3B" stroke-width="2.5"/>
+    <path d="M11 10V0" stroke="#D83D3B" stroke-width="2.5"/>
+    <path d="M2 10V17H20V10" stroke="#D83D3B" stroke-width="2.5"/>
+  </svg>
 </template>
 
 <style scoped lang="scss">
