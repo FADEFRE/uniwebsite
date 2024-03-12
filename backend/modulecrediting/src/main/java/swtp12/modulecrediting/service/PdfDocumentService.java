71841f02cc0bfa60dc4a7d20eecb99a72163c2d8
Frederik Kluge <fk13mami@studserv.uni-leipzig.de>
2024-03-12 18:14:07 +0100

    javadoc #480

--- backend/modulecrediting/src/main/java/swtp12/modulecrediting/service/PdfDocumentService.java
+++ backend/modulecrediting/src/main/java/swtp12/modulecrediting/service/PdfDocumentService.java
@@ -32,16 +32,13 @@ public class PdfDocumentService {
 
     /**
      * This method either gets the {@link PdfDocument} with the given {@code pdfId} or creates a new one.
-     * 
      * @param pdfData {@code MultipartFile} of the {@link PdfDocument} you want to create
      * @param pdfId of the {@link PdfDocument} you want to get
-     * 
-     * @throws ResponseStatusException if both {@code pdfData} and {@code pdfId} are {@code null}
-     * 
+     * @throws ResponseStatusException with {@code HttpStatus.BAD_REQUEST: 400} if both {@code pdfData} and {@code pdfId} are {@code null}
      * @return either the created {@link PdfDocument} or the already existing one with the given {@code pdfId}
-     * 
-     * @see <a href="https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/web/server/ResponseStatusException.html">Spring ResponseStatusException</a>
+     * @see <a href="https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/http/HttpStatus.html">Spring HttpStatus</a>
      * @see <a href="https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/web/multipart/MultipartFile.html">Spring MultipartFile</a>
+     * @see <a href="https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/web/server/ResponseStatusException.html">Spring ResponseStatusException</a>
      */
     public PdfDocument getOrCreatePdfDocument(MultipartFile pdfData, Long pdfId) {
         if (pdfData == null) {
@@ -53,6 +50,15 @@ public class PdfDocumentService {
         return createPdfDocument(pdfData);
     }
 
+    /**
+     * This method creates a {@link PdfDocument} with the given {@code pdfData}
+     * @param pdfData {@code MultipartFile}
+     * @throws ResponseStatusException with {@code HttpStatus.BAD_REQUEST: 400} if {@code pdfData} is {@code null} or {@code empty}
+     * @return the created {@link PdfDocument}
+     * @see <a href="https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/http/HttpStatus.html">Spring HttpStatus</a>
+     * @see <a href="https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/web/multipart/MultipartFile.html">Spring MultipartFile</a>
+     * @see <a href="https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/web/server/ResponseStatusException.html">Spring ResponseStatusException</a>
+     */
     public PdfDocument createPdfDocument(MultipartFile pdfData) {
         if (pdfData == null || pdfData.isEmpty()) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "No PDF file provided or file is empty");
@@ -64,17 +70,40 @@ public class PdfDocumentService {
         }
     }
 
-
+    /**
+     * This method gets the {@code byte[]} of the {@link PdfDocument} with the given {@code id}
+     * @param id {@code Long}
+     * @throws ResponseStatusException with {@code HttpStatus.NOT_FOUND: 404} if the {@link PdfDocument} could not be found
+     * @return the {@code byte[]} of the {@link PdfDocument} with the given {@code id}
+     * @see <a href="https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/http/HttpStatus.html">Spring HttpStatus</a>
+     * @see <a href="https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/web/server/ResponseStatusException.html">Spring ResponseStatusException</a>
+     */
     public byte[] getPdfDocumentDataById(Long id) {
         PdfDocument pdfDocument = getPdfDocumentById(id);
         return pdfDocument.getPdfData();
     }
 
+    /**
+     * This method gets the {@code name} of the {@link PdfDocument} with the given {@code id}
+     * @param id {@code Long}
+     * @throws ResponseStatusException with {@code HttpStatus.NOT_FOUND: 404} if the {@link PdfDocument} could not be found
+     * @return the {@code name} of the {@link PdfDocument} with the given {@code id}
+     * @see <a href="https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/http/HttpStatus.html">Spring HttpStatus</a>
+     * @see <a href="https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/web/server/ResponseStatusException.html">Spring ResponseStatusException</a>
+     */
     public String getPdfDocumentNameById(Long id) {
         PdfDocument pdfDocument = getPdfDocumentById(id);
         return pdfDocument.getName();
     }
 
+    /**
+     * This method gets the {@link PdfDocument} with the given {@code id}
+     * @param id {@code Long}
+     * @throws ResponseStatusException with {@code HttpStatus.NOT_FOUND: 404} if the {@link PdfDocument} could not be found
+     * @return the {@link PdfDocument} with the given {@code id}
+     * @see <a href="https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/http/HttpStatus.html">Spring HttpStatus</a>
+     * @see <a href="https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/web/server/ResponseStatusException.html">Spring ResponseStatusException</a>
+     */
     public PdfDocument getPdfDocumentById(Long id) {
         return pdfDocumentRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "PDF document not found with id: " + id));
     }
