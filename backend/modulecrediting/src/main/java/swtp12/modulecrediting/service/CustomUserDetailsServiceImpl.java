04223c577050316e7edae378c2dc8acd8820ccc5
Frederik Kluge <fk13mami@studserv.uni-leipzig.de>
2024-03-14 00:47:44 +0100

    javadoc #474

--- backend/modulecrediting/src/main/java/swtp12/modulecrediting/service/CustomUserDetailsServiceImpl.java
+++ backend/modulecrediting/src/main/java/swtp12/modulecrediting/service/CustomUserDetailsServiceImpl.java
@@ -10,11 +10,21 @@ import swtp12.modulecrediting.dto.CustomUserDetails;
 import swtp12.modulecrediting.model.User;
 import swtp12.modulecrediting.repository.UserRepository;
 
+/**
+ * This is an implementation of the {@code UserDetailsService}
+ * @author Frederik Kluge
+ * @see #loadUserByUsername
+ * @see <a href="https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/stereotype/Service.html">Spring Service</a>
+ * @see <a href="https://docs.spring.io/spring-security/site/docs/current/api/org/springframework/security/core/userdetails/UserDetailsService.html">Spring UserDetailsService</a>
+ */
 @Service
 public class CustomUserDetailsServiceImpl implements UserDetailsService{
     @Autowired
     private UserRepository userRepository;
 
+    /**
+     * This method overrides the {@code loadUserByUsername}
+     */
     @Override
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(s).orElseThrow(() -> new UsernameNotFoundException("User with this username not found: " + s));
