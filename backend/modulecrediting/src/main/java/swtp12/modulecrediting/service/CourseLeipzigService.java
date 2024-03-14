74497c4d897f8a529b281dfb1fa1e90f7b49c9ce
Frederik Kluge <fk13mami@studserv.uni-leipzig.de>
2024-03-14 01:21:19 +0100

    javadoc #473

--- backend/modulecrediting/src/main/java/swtp12/modulecrediting/service/CourseLeipzigService.java
+++ backend/modulecrediting/src/main/java/swtp12/modulecrediting/service/CourseLeipzigService.java
@@ -20,12 +20,26 @@ import swtp12.modulecrediting.model.ModuleLeipzig;
 import swtp12.modulecrediting.repository.CourseLeipzigRepository;
 import swtp12.modulecrediting.util.LogUtil;
 
-
+/**
+ * This is a {@code Service} for {@link CourseLeipzig}
+ * @author Frederik Kluge
+ * @author Luca Kippe
+ * @see #getAllCoursesLeipzig
+ * @see #getCourseLeipzigByName
+ * @see #getCourseLeipzigNameById
+ * @see #findOrCreateNewCourseLeipzig
+ * @see #saveCourseLeipzigToDatabase
+ * @see #createCourseLeipzig
+ * @see #updateCourseLeipzig
+ * @see #deleteCourseLeipzig
+ * @see #editCourseRelations
+ * @see <a href="https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/stereotype/Service.html">Spring Service</a>
+ */
 @Service
 public class CourseLeipzigService {
+    private ModuleLeipzigService moduleLeipzigService;
+    private ApplicationService applicationService;
     CourseLeipzigRepository courseLeipzigRepository;
-    ModuleLeipzigService moduleLeipzigService;
-    ApplicationService applicationService;
 
     @Autowired
     public CourseLeipzigService(CourseLeipzigRepository courseLeipzigRepository, @Lazy ModuleLeipzigService moduleLeipzigService, @Lazy ApplicationService applicationService) {
@@ -36,20 +50,50 @@ public class CourseLeipzigService {
     public CourseLeipzigService() { } // This cannot!! be a lombok @NoArgsConstructor due to the way @Autowired works in Springboot
 
 
+    /**
+     * This method returns all {@link CourseLeipzig} in the database
+     * @return {@code List} of {@link CourseLeipzig} 
+     * @see CourseLeipzig
+     */
+    public List<CourseLeipzig> getAllCoursesLeipzig() {
+        return courseLeipzigRepository.findAll();
+    }
+
+    /**
+     * This method returns the {@link CourseLeipzig} with the given {@code name}
+     * @param name {@code String}
+     * @throws ResponseStatusException with {@code HttpStatus.NOT_FOUND: 404} if a {@link CourseLeipzig} with the given {@code name} could not be found
+     * @return {@link CourseLeipzig} with the given {@code name}
+     * @see CourseLeipzig
+     * @see <a href="https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/http/HttpStatus.html">Spring HttpStatus</a>
+     * @see <a href="https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/web/server/ResponseStatusException.html">Spring ResponseStatusException</a>
+     */
     public CourseLeipzig getCourseLeipzigByName(String name) {
         CourseLeipzig courseLeipzig = courseLeipzigRepository.findByName(name).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Course Leipzig not found with given name: " + name));
         return courseLeipzig;
     }
 
+    /**
+     * This method returns the {@code name} of the {@link CourseLeipzig} with the given {@code id}
+     * @param id {@code Long}
+     * @throws ResponseStatusException with {@code HttpStatus.NOT_FOUND: 404} if a {@link CourseLeipzig} with the given {@code id} could not be found
+     * @return {@code name} of the {@link CourseLeipzig} with the given {@code id}
+     * @see CourseLeipzig
+     * @see <a href="https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/http/HttpStatus.html">Spring HttpStatus</a>
+     * @see <a href="https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/web/server/ResponseStatusException.html">Spring ResponseStatusException</a>
+     */
     public String getCourseLeipzigNameById(Long id) {
         CourseLeipzig courseLeipzig = courseLeipzigRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Course Leipzig not found with given id: " + id));
         return courseLeipzig.getName();
     }
 
-    public List<CourseLeipzig> getAllCoursesLeipzig() {
-        return courseLeipzigRepository.findAll();
-    }
-
+    /**
+     * This method gets the {@link CourseLeipzig} with the given {@code name} or creates it
+     * <p> If found, {@link CourseLeipzig} will be set as {@code isActive}
+     * @param name {@code String}
+     * @return {@link CourseLeipzig} that has been found or created
+     * @see CourseLeipzig
+     */
     public CourseLeipzig findOrCreateNewCourseLeipzig(String name) {
         CourseLeipzig courseLeipzig = courseLeipzigRepository.findByName(name).orElseGet(() -> {
             return courseLeipzigRepository.save(new CourseLeipzig(name));
@@ -59,15 +103,31 @@ public class CourseLeipzigService {
         return courseLeipzig;
     }
 
+    /**
+     * This method saves the given {@link CourseLeipzig} to the database
+     * @param courseLeipzig {@link CourseLeipzig}
+     * @return {@link CourseLeipzig} that has been saved
+     * @see CourseLeipzig
+     */
     public CourseLeipzig saveCourseLeipzigToDatabase(CourseLeipzig courseLeipzig) {
         return courseLeipzigRepository.save(courseLeipzig);
     }
 
+    /**
+     * This method creates a new {@link CourseLeipzig} definied in the given {@link CourseLeipzigDTO}
+     * @param courseLeipzigDTO {@link CourseLeipzigDTO}
+     * @throws ResponseStatusException with {@code HttpStatus.BAD_REQUEST: 400} if the {@link CourseLeipzigDTO} is {@code null}
+     * @throws ResponseStatusException with {@code HttpStatus.BAD_REQUEST: 400} if the {@code courseName} field in the {@link CourseLeipzigDTO} is {@code null} or {@code blank}
+     * @throws ResponseStatusException with {@code HttpStatus.CONFLICT: 409} if a {@link CourseLeipzig} with the given {@code name} already exists and is active
+     * @return {@code name} of the created {@link CourseLeipzig}
+     * @see CourseLeipzig
+     * @see CourseLeipzigDTO
+     * @see <a href="https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/http/HttpStatus.html">Spring HttpStatus</a>
+     * @see <a href="https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/web/server/ResponseStatusException.html">Spring ResponseStatusException</a>
+     */
     public String createCourseLeipzig(CourseLeipzigDTO courseLeipzigDTO) {
-        if (courseLeipzigDTO == null)
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "No data given");
-        if (courseLeipzigDTO.getCourseName() == null || courseLeipzigDTO.getCourseName().isBlank())
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "No course name given");
+        if (courseLeipzigDTO == null) throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "No data given");
+        if (courseLeipzigDTO.getCourseName() == null || courseLeipzigDTO.getCourseName().isBlank()) throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "No course name given");
 
         Optional<CourseLeipzig> courseLeipzigOptional = courseLeipzigRepository.findByName(courseLeipzigDTO.getCourseName());
         if (courseLeipzigOptional.isPresent()) {
@@ -87,11 +147,23 @@ public class CourseLeipzigService {
         return courseLeipzig.getName();
     }
 
+    /**
+     * This method updates the {@link CourseLeipzig} with the given {@code name} with data definied in the given {@link CourseLeipzigDTO}
+     * @param courseName {@code String}
+     * @param courseLeipzigDTO {@link CourseLeipzigDTO}
+     * @throws ResponseStatusException with {@code HttpStatus.BAD_REQUEST: 400} if the {@link CourseLeipzigDTO} is {@code null}
+     * @throws ResponseStatusException with {@code HttpStatus.BAD_REQUEST: 400} if the {@code courseName} field in the {@link CourseLeipzigDTO} is {@code null} or {@code blank}
+     * @throws ResponseStatusException with {@code HttpStatus.BAD_REQUEST: 400} if the to be updated {@link CourseLeipzig} is inactive
+     * @throws ResponseStatusException with {@code HttpStatus.CONFLICT: 409} if a {@link CourseLeipzig} with the possible new {@code name} already exists
+     * @return the (possible new) {@code name} of the updated {@link CourseLeipzig}
+     * @see CourseLeipzig
+     * @see CourseLeipzigDTO
+     * @see <a href="https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/http/HttpStatus.html">Spring HttpStatus</a>
+     * @see <a href="https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/web/server/ResponseStatusException.html">Spring ResponseStatusException</a>
+     */
     public String updateCourseLeipzig(String courseName, CourseLeipzigDTO courseLeipzigDTO) {
-        if (courseLeipzigDTO == null)
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "No data given");
-        if (courseLeipzigDTO.getCourseName() == null || courseLeipzigDTO.getCourseName().isBlank())
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "No course name given");
+        if (courseLeipzigDTO == null) throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "No data given");
+        if (courseLeipzigDTO.getCourseName() == null || courseLeipzigDTO.getCourseName().isBlank()) throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "No course name given");
 
         CourseLeipzig courseLeipzig = getCourseLeipzigByName(courseName);
 
@@ -108,7 +180,16 @@ public class CourseLeipzigService {
         return courseLeipzig.getName();
     }
 
-
+    /**
+     * This method deletes or deactivates the {@link CourseLeipzig} with the given {@code name}. Deactivation happens if the {@link CourseLeipzig} has been used in a {@link Application}
+     * @param name {@code String}
+     * @throws ResponseStatusException with {@code HttpStatus.BAD_REQUEST: 400} if the {@link CourseLeipzig} is already deactivated
+     * @return {@code String} of the operation executed
+     * @see Application
+     * @see CourseLeipzig
+     * @see <a href="https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/http/HttpStatus.html">Spring HttpStatus</a>
+     * @see <a href="https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/web/server/ResponseStatusException.html">Spring ResponseStatusException</a>
+     */
     public String deleteCourseLeipzig(String name) {
         CourseLeipzig courseLeipzig = getCourseLeipzigByName(name);
         if (!courseLeipzig.getIsActive())
@@ -127,18 +208,22 @@ public class CourseLeipzigService {
         return "DEACTIVATED";
     }
 
-    private Boolean checkIfCourseIsUsedInApplications(CourseLeipzig courseLeipzig) {
-        List<Application> applications = applicationService.getAllApplciations();
-
-        if(applications.isEmpty()) return false;
-
-        for(Application application : applications) {
-            if(courseLeipzig.equals(application.getCourseLeipzig())) return true;
-        }
-
-        return false;
-    }
-
+    /**
+     * This method edits which {@link ModuleLeipzig} are part of the {@link CourseLeipzig} with the given {@code courseName}
+     * @param courseName {@code String}
+     * @param editCourseRelationsDTO {@link CourseLeipzigRelationEditDTO}
+     * @throws ResponseStatusException with {@code HttpStatus.BAD_REQUEST: 400} if the {@link CourseLeipzigRelationEditDTO} is {@code null}
+     * @throws ResponseStatusException with {@code HttpStatus.BAD_REQUEST: 400} if the {@code courseName} is {@code null}
+     * @throws ResponseStatusException with {@code HttpStatus.BAD_REQUEST: 400} if the to be updated {@link CourseLeipzig} is inactive
+     * @throws ResponseStatusException with {@code HttpStatus.BAD_REQUEST: 400} if the {@code name} or {@code code} of any of the to be updated {@link ModuleLeipzig} is {@code null}
+     * @throws ResponseStatusException with {@code HttpStatus.BAD_REQUEST: 400} if the {@code name} and {@code code} of any of the to be updated {@link ModuleLeipzig} do not correspond to the same {@link ModuleLeipzig}
+     * @return {@code name} of the edited {@link CourseLeipzig}
+     * @see CourseLeipzig
+     * @see CourseLeipzigRelationEditDTO
+     * @see ModuleLeipzig
+     * @see <a href="https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/http/HttpStatus.html">Spring HttpStatus</a>
+     * @see <a href="https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/web/server/ResponseStatusException.html">Spring ResponseStatusException</a>
+     */
     public String editCourseRelations(String courseName, CourseLeipzigRelationEditDTO editCourseRelationsDTO) {
         if(editCourseRelationsDTO == null) throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "No data given");
         if(courseName == null) throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "No Course Name sent");
@@ -162,7 +247,7 @@ public class CourseLeipzigService {
                 if(!moduleLeipzig.getCode().equals(mLDTO.getCode()))
                     throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Module Code doesn't match: " + mLDTO.getName() + " <-> " + mLDTO.getCode());
 
-                modulesLeipzigToDelete = removeModulesFromList(modulesLeipzigToDelete, moduleLeipzig);
+                modulesLeipzigToDelete = moduleLeipzigService.removeModulesFromList(modulesLeipzigToDelete, moduleLeipzig);
                 modulesLeipzigToAdd.add(moduleLeipzig);
             }
         }
@@ -173,7 +258,7 @@ public class CourseLeipzigService {
         }
         
         for (ModuleLeipzig moduleLeipzig : courseLeipzig.getModulesLeipzigCourse()) {
-            modulesLeipzigToAdd = removeModulesFromList(modulesLeipzigToAdd, moduleLeipzig);
+            modulesLeipzigToAdd = moduleLeipzigService.removeModulesFromList(modulesLeipzigToAdd, moduleLeipzig);
         }
 
         for (ModuleLeipzig moduleLeipzig : modulesLeipzigToAdd) {
@@ -185,13 +270,26 @@ public class CourseLeipzigService {
         return courseLeipzig.getName();
     }
 
-    private List<ModuleLeipzig> removeModulesFromList(List<ModuleLeipzig> moduleList, ModuleLeipzig moduleLeipzig) {
-        for (int i = 0; i < moduleList.size(); i++) {
-            ModuleLeipzig mL = moduleList.get(i);
-            if (moduleLeipzig.getName().equals(mL.getName())) {
-                moduleList.remove(i);
-            }
+    
+    // ------- Private Methods -------
+
+    /**
+     * This method checks if the given {@link CourseLeipzig} is used in any {@link Application}
+     * @param courseLeipzig {@link CourseLeipzig}
+     * @return {@code True} if the given {@link CourseLeipzig} is used in an {@link Application}
+     * @see Application
+     * @see CourseLeipzig
+     */
+    private Boolean checkIfCourseIsUsedInApplications(CourseLeipzig courseLeipzig) {
+        List<Application> applications = applicationService.getAllApplciations();
+
+        if(applications.isEmpty()) return false;
+
+        for(Application application : applications) {
+            if(courseLeipzig.equals(application.getCourseLeipzig())) return true;
         }
-        return moduleList;
+
+        return false;
     }
+
 }
